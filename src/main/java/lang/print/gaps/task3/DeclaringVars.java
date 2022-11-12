ad2336107cab14561c1ad69fcf8562c96e3a707e
Husanboy <olimboyevhusanboy@gmail.com>
2022-11-12 16:13:35 +0500

    okk

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -5,8 +5,8 @@ public class DeclaringVars {
         //write code here
         int a,b,c;
         a=10;
-        b=1;
-        c=0;
+        b=10;
+        c=1;
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
